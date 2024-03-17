f8f042309a3a076c650ce150fd1d479a577113be
AyoubAnej <alyapani9@gmail.com>
2024-03-17 02:58:45 +0000

    first commit

---
+++ src/main/java/org/example/security/demo/DemoController.java
@@ -0,0 +1,14 @@
+package org.example.security.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("api/v1/demo-controller")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello from secured endpoint");
+    }
+}
